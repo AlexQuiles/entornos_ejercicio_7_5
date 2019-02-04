6a1858cb6949b014cc972eda8f645dd8c90b80aa
Alex <Alex@10.0.2.15>
2019-02-04 18:06:13 +0100

    Modificada rama issue54, añadido verde

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -17,6 +17,7 @@ public class HolaMundo {
     public static void main(String[] args) {  
         System.out.println("Holi Mundiiii");// TODO code application logic here
         System.out.println("ROJO");
+        System.out.println("VERDE");
         
     }
     
