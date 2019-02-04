1d6dba63fd4dcb68e3a6836c4cba0dde4c36d29f
Alex <Alex@10.0.2.15>
2019-02-04 17:45:05 +0100

    Modificacion de hola mundo

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {  
-        System.out.println("Hola Mundo");// TODO code application logic here
+        System.out.println("Holi Mundiiii");// TODO code application logic here
     }
     
 }
